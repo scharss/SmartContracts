693448d670e0ccaab88e3589c5cf6a69ca6d5bcd
Scharss <scharss@gmail.com>
2022-08-10 21:13:37 -0500

    Create revertMinter.sol

---
+++ revertMinter.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts@4.7.2/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts@4.7.2/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts@4.7.2/access/Ownable.sol";
+
+contract RevertMinter is ERC1155, ERC1155Burnable, Ownable {
+    constructor() ERC1155("https://ipfs.io/ipfs/bafybeigutzu3ccjxhnrkv3cqpwbeltryjr4m43plgra5cols34swqtkvje/{id}.json") {}
+
+     uint256 public number =1;
+     uint256 public num;
+
+   
+
+    function mint()
+        public
+        onlyOwner
+    {
+        if(number<=10000){
+       num = number;
+        
+          _mint(msg.sender, num /* Id  */, 1 /* Cantidad */, "");
+        number= number +1;
+
+        }else{
+            revert("Todo ha sido vendido");
+        }
+
+        
+    }
+
+    
+}
