53ee75f1dd49cb0182dc27555b4b6f4bab30fa79
Scharss <scharss@gmail.com>
2022-08-10 20:43:05 -0500

    Update and rename testMinter.sol to candymachine.sol

--- candymachine.sol
+++ candymachine.sol
@@ -5,24 +5,22 @@ import "@openzeppelin/contracts@4.7.2/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts@4.7.2/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts@4.7.2/access/Ownable.sol";
 
-contract Minter is ERC1155, ERC1155Burnable, Ownable {
+contract NuevoMinter is ERC1155, ERC1155Burnable, Ownable {
     constructor() ERC1155("https://ipfs.io/ipfs/bafybeigutzu3ccjxhnrkv3cqpwbeltryjr4m43plgra5cols34swqtkvje/{id}.json") {}
 
-     uint256 public number = 0;
-    
+     uint256 public number =1;
+     uint256 public num;
 
-    function storex() public {
-        number = number +1;
-        
-    }
+   
 
     function mint()
         public
         onlyOwner
     {
-        for(uint i=number; i<=numbery; i++){
-          _mint(msg.sender, i /* Id  */, 1 /* Cantidad */, "");
-        }
+        num = number;
+        
+          _mint(msg.sender, num /* Id  */, 1 /* Cantidad */, "");
+        number= number +1;
     }
 
     
