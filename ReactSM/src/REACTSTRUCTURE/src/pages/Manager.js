20bc1ba215d2d91e324968ecac5c2d7ff215e2b9
Scharss <scharss@gmail.com>
2022-08-23 14:46:59 -0500

    Add files via upload

---
+++ ReactSM/src/REACTSTRUCTURE/src/pages/Manager.js
@@ -0,0 +1,15 @@
+import {ContractManager,Direccion, ReadContract, WriteContract} from "./ContractManager";
+
+
+export function Manager(){
+	return(<div>
+
+
+	<ContractManager />
+	<Direccion />
+	<ReadContract />
+	<WriteContract />
+	
+	</div>
+);
+}
